a12ff8f327a3f848e3f2fe8d501890aef312e1ec
Nastija <nastija.sij@gmail.com>
2025-04-18 13:28:14 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather Search Engine</h1>
+        </header>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Nastija-Sij" target="_blank">
+            Nastija Sij
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Nastija-Sij/react-weather-search-engine"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
