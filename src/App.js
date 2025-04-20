b60e098d99286b74e152b7864d2546f40645838a
Nastija <nastija.sij@gmail.com>
2025-04-20 20:39:49 +0200

    added rel noreferrer to appjs

--- src/App.js
+++ src/App.js
@@ -3,29 +3,31 @@ import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
- 
-    return (
-      <div className="App">
-        <div className="weather-app-container">
-
-          <main>
-            <Weather defaultCity="Vienna" />
-          </main>
-          <footer>
-            This project was coded by{" "}
-            <a href="https://github.com/Nastija-Sij" target="_blank">
-              Nastija Sij
-            </a>{" "}
-            and is open-sourced on{" "}
-            <a
-              href="https://github.com/Nastija-Sij/react-weather-search-engine"
-              target="_blank"
-            >
-              GitHub
-            </a>
-          </footer>
-        </div>
+  return (
+    <div className="App">
+      <div className="weather-app-container">
+        <main>
+          <Weather defaultCity="Vienna" />
+        </main>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Nastija-Sij"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nastija Sij
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Nastija-Sij/react-weather-search-engine"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
       </div>
-    );
-  }
-
+    </div>
+  );
+}
